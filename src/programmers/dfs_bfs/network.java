7dc7c65a44ac095a533e4767228a9b6c5021963c
jjaekkaemi <o0o3310@hanmail.net>
2024-04-08 23:10:18 +0900

    init

---
+++ src/programmers/dfs_bfs/network.java
@@ -0,0 +1,23 @@
+package programmers.dfs_bfs;
+
+class Solution {
+    public void dfs(int i, int[][] computers, boolean[] visited){
+        visited[i] = true;
+        for(int j = 0 ; j<computers[i].length ; j++){
+            if(i!=j && computers[i][j]==1 && !visited[j]){
+                dfs(j, computers, visited);
+            }
+        }
+    }
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for(int i = 0 ; i<computers.length ; i++){
+            if(!visited[i]){
+                dfs(i, computers, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
