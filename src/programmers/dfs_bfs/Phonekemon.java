4dc6cbb27495fea2afd0282aca3095bcac0d21c4
jjaekkaemi <o0o3310@hanmail.net>
2024-04-12 00:13:05 +0900

    프로그래머스 폰켓몬 (HashMap)

---
+++ src/programmers/dfs_bfs/Phonekemon.java
@@ -0,0 +1,16 @@
+package programmers.dfs_bfs;
+import java.util.*;
+public class Phonekemon {
+    class Solution {
+        public int solution(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int num : nums){
+                map.put(num,0);
+            }
+            if(map.size()>nums.length/2){
+                return nums.length/2 ;
+            }
+            return map.size();
+        }
+    }
+}
